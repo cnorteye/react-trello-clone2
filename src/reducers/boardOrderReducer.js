161addbe5fd1f9a1ae2d4c9b856f0f75400c5e81
Comfort <65386761+cnorteye@users.noreply.github.com>
2020-08-19 09:24:45 -0700

    trello-clone project intial commit

---
+++ src/reducers/boardOrderReducer.js
@@ -0,0 +1,18 @@
+import { CONSTANTS } from "../actions";
+import { v4 as uuidv4 } from 'uuid';
+
+console.log(uuidv4());
+
+const initialState = ["board-0"];
+
+const boardOrderReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CONSTANTS.ADD_BOARD: {
+      return [...state, `board-${action.payload.id}`];
+    }
+    default:
+      return state;
+  }
+};
+
+export default boardOrderReducer;
